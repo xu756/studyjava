dadbe050da14dea588506bba39d10ea0d5f20776
xu756 <756334744@qq.com>
2022-06-26 18:09:16 +0800

    初始化

--- src/hello/helloword.java
+++ src/hello/helloword.java
@@ -1,3 +1,5 @@
+package src.hello;
+
 public class helloword {
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ src/oneday/oneday.java
@@ -0,0 +1,17 @@
+package src.oneday;
+
+public class oneday {
+    String name;
+    int age;
+
+    public oneday(String name, int age) {
+        this.name = name;       // this.name is the name of the variable
+        this.age = age;         // this.age is the name of the variable
+    }
+
+    public static void main(String[] args) {
+    test test = new test();     //调用test类的构造函数
+    test.funtest1();   //调用test类的funtest函数
+    }
+
+}
